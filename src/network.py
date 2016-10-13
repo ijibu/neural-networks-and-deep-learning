604235a0a3ee36da4574d24b17ea03bdabd09471
ijibu <ijibu.com@gmail.com>
2016-10-13 17:54:20 +0800

    添加代码注释 Signed-off-by: ijibu <ijibu.com@gmail.com>

--- src/network.py
+++ src/network.py
@@ -28,7 +28,9 @@ class Network(object):
         distribution with mean 0, and variance 1.  Note that the first
         layer is assumed to be an input layer, and by convention we
         won't set any biases for those neurons, since biases are only
-        ever used in computing the outputs from later layers."""
+        ever used in computing the outputs from later layers.
+		randn(y, x)函数生成一些正态分布的随机数组（y行x列），函数生成均值为0，标准差为1的高斯分布
+		"""
         self.num_layers = len(sizes)
         self.sizes = sizes
         self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
@@ -36,7 +38,9 @@ class Network(object):
                         for x, y in zip(sizes[:-1], sizes[1:])]
 
     def feedforward(self, a):
-        """Return the output of the network if ``a`` is input."""
+        """Return the output of the network if ``a`` is input.
+		np.dot(w, a)表示向量(矩阵)的乘法，w和a向量相乘。对神经网络的每一层应用np.dot(w, a)+b
+		"""
         for b, w in zip(self.biases, self.weights):
             a = sigmoid(np.dot(w, a)+b)
         return a
@@ -50,7 +54,15 @@ class Network(object):
         self-explanatory.  If ``test_data`` is provided then the
         network will be evaluated against the test data after each
         epoch, and partial progress printed out.  This is useful for
-        tracking progress, but slows things down substantially."""
+        tracking progress, but slows things down substantially.
+		随机梯度下降算法
+		training_data：是一个代表着训练输入和对应的期望输出的元组（x,y）的列表
+		变量epochs和mini_batch_size是你期望的训练的迭代次数和取样时所用的mini-batch块的大小。
+		eta是学习率η
+		如果可选参数test_data被提供，那么程序将会在每次训练迭代之后评价网络，并输出我们的局部进展。
+		xrange(0, n, mini_batch_size)从0开始，步长为mini_batch_size，不超过n，生成一系列数字。
+		xrange(0, 100, 30)---->(0, 30, 60, 90)
+		"""
         if test_data: n_test = len(test_data)
         n = len(training_data)
         for j in xrange(epochs):
@@ -70,7 +82,9 @@ class Network(object):
         """Update the network's weights and biases by applying
         gradient descent using backpropagation to a single mini batch.
         The ``mini_batch`` is a list of tuples ``(x, y)``, and ``eta``
-        is the learning rate."""
+        is the learning rate.
+		仅仅使用mini_batch上的训练数据，根据单轮梯度下降更新网络的权重和偏差。
+		"""
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
         for x, y in mini_batch:
@@ -86,7 +100,8 @@ class Network(object):
         """Return a tuple ``(nabla_b, nabla_w)`` representing the
         gradient for the cost function C_x.  ``nabla_b`` and
         ``nabla_w`` are layer-by-layer lists of numpy arrays, similar
-        to ``self.biases`` and ``self.weights``."""
+        to ``self.biases`` and ``self.weights``.
+		反向传播（backpropagation）的算法，这是一种快速计算代价函数的梯度的方法"""
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
         # feedforward
@@ -121,7 +136,9 @@ class Network(object):
         """Return the number of test inputs for which the neural
         network outputs the correct result. Note that the neural
         network's output is assumed to be the index of whichever
-        neuron in the final layer has the highest activation."""
+        neuron in the final layer has the highest activation.
+		np.argmax(x, y) 返回最大值的下标
+		"""
         test_results = [(np.argmax(self.feedforward(x)), y)
                         for (x, y) in test_data]
         return sum(int(x == y) for (x, y) in test_results)
